11ea3b8dd352ef7bb355b958cbafa11da267cef5
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-01-07 12:26:06 -0200

    Update to django 1.6

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,5 +1,6 @@
 import os
+from django import VERSION
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response('home/home.html')
+    return render_to_response('home/home.html', {'django_version': '.'.join([str(i) for i in VERSION[:3]])})
