8becd813e0e56d0d6ff8428b65e744503cd6e4f2
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-05-29 13:05:01 -0300

    Defaults to django-1.7.x

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@
 from setuptools import setup
 import sys
 
-if (sys.version_info.major == 2 and sys.version_info.minor == 7) or sys.version_info.major == 3:
-    django = 17
-else:
-    django = 16    
-
 setup(
     name='YourAppName',
     version='1.0',
@@ -15,5 +10,4 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django >=1.6, <1.7' if django == 16 else 'Django >=1.7'],
 )
